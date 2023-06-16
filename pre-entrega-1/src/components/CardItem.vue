918f9144d5e8c41026cd93333af8d452cc0c1f0d
Andres-Developer <andres.burbano.dev@gmail.com>
2023-06-15 22:25:55 -0500

    pre-entrega-1

---
+++ pre-entrega-1/src/components/CardItem.vue
@@ -0,0 +1,37 @@
+<template>
+
+</template>
+
+<script>
+// import ComponentName from './ComponentName.vue'
+
+export default {
+name: 'CardItem',
+  components: {
+    // ComponentName
+  },
+  props: {
+    // someProp: String
+  },
+  data() { 
+   return {
+   }
+  },
+
+  methods: {
+    /* someMethod() {
+      this.someData
+    } */
+  },
+  computed: {
+    /* someComputed() {
+      return this.someData
+    } */
+  },
+
+};
+</script>
+
+<style scoped>
+
+</style>
