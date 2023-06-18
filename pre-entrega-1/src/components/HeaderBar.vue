e11ecdeed300de0e60b861ed4f0a3cdae4528116
Andres-Developer <andres.burbano.dev@gmail.com>
2023-06-18 01:50:12 -0500

    Fix height and delete Toggle button

--- pre-entrega-1/src/components/HeaderBar.vue
+++ pre-entrega-1/src/components/HeaderBar.vue
@@ -1,21 +1,18 @@
 <template>
-  <div class="bg-warning">
-    <b-navbar class="container" toggleable="lg" type="dark" variant="warning">
+  <b-navbar class="fixed-top" toggleable="lg" type="dark" variant="warning">
+    <div class="container d-flex">
       <b-navbar-brand to="/" href="#" class="fs-3 text-white fw-bold">Pizzería</b-navbar-brand>
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-      <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav class="ms-auto">
-          <b-nav-item :to="{ name: 'cart-content' }" href="#" class="me-auto">
-            <img alt="cart" src="./../assets/cart-header.svg" width="35">
-            <span v-if="count > 0" class="position-absolute top-3  translate-middle badge rounded-pill bg-danger">
-              {{ count }}
-            </span>
-          </b-nav-item>
-          <!-- <b-nav-item :to="{ name: 'post-id', params: { id: 1 } }" href="#" class="me-auto">Post numero 1</b-nav-item> -->
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
-  </div>
+      <b-navbar-nav class="ms-auto">
+        <b-nav-item :to="{ name: 'cart-content' }" href="#" class="me-auto">
+          <img alt="cart" src="./../assets/cart-header.svg" width="35">
+          <span v-if="count > 0" class="position-absolute top-3  translate-middle badge rounded-pill bg-danger">
+            {{ count }}
+          </span>
+        </b-nav-item>
+      </b-navbar-nav>
+    </div>
+  </b-navbar>
+  <div id="fixOffset"></div>
 </template>
 
 <script>
@@ -27,4 +24,8 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+#fixOffset {
+  height: 66px;
+}
+</style>
