1404886885fe44788a6642a7c163557c5cf310c9
Andres-Developer <andres.burbano.dev@gmail.com>
2023-06-19 00:55:17 -0500

    mod: styles, buttons, texts

--- pre-entrega-1/src/views/ProductDetail.vue
+++ pre-entrega-1/src/views/ProductDetail.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="custom-height container d-flex justify-content-center align-items-center">
-    <b-card no-body class="overflow-hidden" style="width: 640px; height: 400px;">
+    <b-card no-body class="overflow-hidden" style="width: 640px; height: 500px;">
       <b-row class="g-0">
         <b-col md="6">
           <b-card-img :src="product.image" alt="Image" class="rounded-0" />
@@ -11,14 +11,16 @@
               {{ product.description }}
 
               <div v-if="checkSelectedProduct">
-                <div class="mt-4">Has añadido <strong>{{ localQuantity }} </strong> pizzas al carrito por:</div>
+                <div class="mt-4 mb-3 text-success">Has añadido <strong>{{ localQuantity }} </strong> pizzas al carrito por:</div>
                 <div class="h4"> $ {{ getProductFromCart.subtotal }}</div>
-                <b-button variant="success" style="pointer-events: none;">Agregada!</b-button>
-                <div type="button" @click="[deleteToCartHandleClick(product.id)]">X Eliminar</div>
+                <div class="d-flex justify-content-center align-items-center gap-2">
+                  <div type="button" @click="[deleteToCartHandleClick(product.id)]" class="text-danger">🗑️ Eliminar</div>
+                </div>
+                <b-button to="/cart" class="mt-5" variant="success"> 🛒 ir al carrito</b-button>
               </div>
               <div v-else>
 
-                <div class="d-flex justify-content-center align-items-center">
+                <div class="d-flex justify-content-center align-items-center mt-5">
                   <b-button @click="substractLocalQuantity" variant="light">
                     -
                   </b-button>
@@ -28,7 +30,7 @@
                   </b-button>
                   <div class="fs-3"> $ {{ localPrice || product.price }}</div>
                 </div>
-                <b-button variant="danger" @click="[addToCartHandleClick({ ...product, quantity: localQuantity })]">
+                <b-button variant="danger" @click="[addToCartHandleClick({ ...product, quantity: localQuantity })]" class="mt-5">
                   <div class="d-flex justify-content-center align-items-center gap-2">
                     <img alt="add to cart" src="./../assets/add-to-cart.svg" width="25">
                     <div class="fs-5"> Agregar al carrito </div>
