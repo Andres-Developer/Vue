09320f50c46e1cadc106809fecefae287c89b79f
Andres-Developer <andres.burbano.dev@gmail.com>
2023-06-19 23:48:14 -0500

    new feature: register component

--- pre-entrega-1/src/components/HeaderBar.vue
+++ pre-entrega-1/src/components/HeaderBar.vue
@@ -1,14 +1,22 @@
 <template>
   <b-navbar class="fixed-top" toggleable="lg" type="dark" variant="warning">
     <div class="container d-flex">
-      <b-navbar-brand to="/" href="#" class="fs-3 text-white fw-bold">Pizzería</b-navbar-brand>
+      <b-navbar-brand to="/" class="fs-3 text-white fw-bold">Pizzería</b-navbar-brand>
       <b-navbar-nav class="ms-auto">
-        <b-nav-item :to="{ name: 'cart-content' }" href="#" class="me-auto">
-          <img alt="cart" src="./../assets/cart-header.svg" width="35">
-          <span v-if="count > 0" class="position-absolute top-3  translate-middle badge rounded-pill bg-danger">
-            {{ count }}
-          </span>
-        </b-nav-item>
+        <div class="d-flex align-items-center">
+          <!-- <b-nav-item :to="{ name: 'login-user' }" class="me-auto">
+            Login
+          </b-nav-item> -->
+          <b-nav-item :to="{ name: 'register-user' }" class="me-auto">
+            Registrarse
+          </b-nav-item>
+          <b-nav-item :to="{ name: 'cart-content' }" class="me-auto">
+            <img alt="cart" src="./../assets/cart-header.svg" width="35">
+            <span v-if="count > 0" class="position-absolute top-3  translate-middle badge rounded-pill bg-danger">
+              {{ count }}
+            </span>
+          </b-nav-item>
+        </div>
       </b-navbar-nav>
     </div>
   </b-navbar>
