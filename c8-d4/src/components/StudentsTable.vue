f8cbfcc4e10458dec69dc8c66dd86efd066bce84
Andres-Developer <andres.burbano.dev@gmail.com>
2023-06-20 18:05:24 -0500

    Desafío 4 - Formularios

---
+++ c8-d4/src/components/StudentsTable.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <h2 class="">Estudiantes registrados</h2>
+    <div class="outer-container-table mt-4">
+      <b-table-simple responsive class="container table-width">
+        <b-thead>
+          <b-tr variant="light">
+            <b-th>Id</b-th>
+            <b-th>Nombre</b-th>
+            <b-th>E-mail</b-th>
+            <b-th>Género</b-th>
+            <b-th>Cursos inscritos</b-th>
+          </b-tr>
+        </b-thead>
+        <b-tbody>
+          <b-tr v-for="student in students" :key="student.id" class="align-middle">
+            <b-td sticky-column> {{ student.id }}</b-td>
+            <b-td> {{ capitalizeWord(student.name) }} </b-td>
+            <b-td> {{ student.email }}</b-td>
+            <b-td> {{ getSpanishGenders(student.gender) }}</b-td>
+            <b-td>
+              <span v-for="course, index in student.courses" :key="index">{{ capitalizeWord(course) }}, </span>
+            </b-td>
+          </b-tr>
+        </b-tbody>
+        <b-tfoot>
+          <b-tr variant="light">
+            <b-th></b-th>
+            <b-th colspan="2" class="text-end">Total alumnos: {{ this.students.length }}</b-th>
+            <b-th></b-th>
+            <b-th></b-th>
+          </b-tr>
+        </b-tfoot>
+      </b-table-simple>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'StudentsTable',
+  components: {
+  },
+  props: {
+    students: Array,
+  },
+  data() {
+    return {
+    };
+  },
+  methods: {
+    getSpanishGenders(gender) {
+      if (gender === "male") {
+        return 'Masculino';
+      }
+      if (gender === "female") {
+        return 'Femenino';
+      }
+      if (gender === "non_binary") {
+        return 'No binario';
+      }
+      return 'Prefiere no decirlo';
+    },
+    capitalizeWord(word) {
+      return (word && word[0].toUpperCase() + word.slice(1)) || "";
+    }
+  },
+  computed: {
+  },
+};
+</script>
+
+<style scoped>
+.outer-container-table {
+  width: 950px;
+  height: auto;
+  border: 1px solid #e4e4e4;
+
+  padding: 1.5em;
+  border-radius: 1em;
+  margin: 0 auto 1em auto;
+}
+</style>
---
+++ c8-d4/src/main.js
@@ -0,0 +1,35 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+
+import BootstrapVueNext from 'bootstrap-vue-next';
+import { plugin, defaultConfig } from "@formkit/vue";
+import "@formkit/themes/genesis";
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css';
+
+createApp(App)
+  .use(BootstrapVueNext)
+  .use(plugin, defaultConfig({
+    config: {
+      classes: {
+        wrapper: {
+          'my-wrapper': true,
+        },
+        fieldset: {
+          'my-fieldset': true,
+        },
+        label: {
+          'my-label': true
+        },
+        help: {
+          'my-help': true
+        },
+        legend: {
+          'my-legend': true,
+          'float-none': true,
+        },
+      }
+    }
+  }))
+  .mount('#app');
