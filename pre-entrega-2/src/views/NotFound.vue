cd3f868d8341bca01cc0cdd2bcbf885ebbcc6546
Andres-Developer <andres.burbano.dev@gmail.com>
2023-07-03 12:44:39 -0500

    mod: 404 not found view

--- pre-entrega-2/src/views/NotFound.vue
+++ pre-entrega-2/src/views/NotFound.vue
@@ -1,5 +1,9 @@
 <template>
-  <div> 404 NOT FOUND :(</div>
+  <div class="custom-height d-flex flex-column justify-content-center align-items-center text-big">
+    <div>404</div>
+    <div>Ruta no encontrada</div>
+    <button type="button" class="btn btn-primary mt-5 btn-chip" @click="() => $router.push('/')">Volver a productos</button>
+  </div>
 </template>
 
 <script>
@@ -20,4 +24,18 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.custom-height {
+  height: calc(100vh - 66px)
+}
+
+.text-big {
+  font-size: 5rem;
+  line-height: 5.5rem;
+}
+.btn-chip {
+  padding: 1rem 2rem;
+  border-radius: 4rem;
+  font-size: 2rem  
+}
+</style>
