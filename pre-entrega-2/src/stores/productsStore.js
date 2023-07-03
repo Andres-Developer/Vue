25a0acadf40d861b9809db5f55221f3223cb7976
Andres-Developer <andres.burbano.dev@gmail.com>
2023-07-03 03:16:33 -0500

    mod: Products Store: delete products methods

--- pre-entrega-2/src/stores/productsStore.js
+++ pre-entrega-2/src/stores/productsStore.js
@@ -1,4 +1,4 @@
-import { getRequest }  from '../services/httpRequests';
+import { deleteRequest, getRequest } from '../services/httpRequests';
 import { loadingWithTimeout } from '../utils/loadingTools';
 
 const productsStore = {
@@ -12,6 +12,26 @@ const productsStore = {
     this.loading = true;
     this.products = await getRequest(BASE_URL + ENDPOINT);
     this.loading = await loadingWithTimeout(50);
+  },
+  async deleteProduct(id) {
+    console.log("entró a deleteProduct");
+    const response = await this.deleteProductFromAPI(id);
+    console.log("respone es: ", response);
+    const index = this.getIndexOfProduct(id);
+    this.products.splice(index, 1);
+    return response;
+  },
+  getIndexOfProduct(id) {
+    return this.products.findIndex((Eproduct) => Eproduct.id === id);
+  },
+
+  async deleteProductFromAPI(id) {
+    const BASE_URL = process.env.VUE_APP_BASE_URL;
+    const ENDPOINT = `/products/${id}`;
+    this.loading = true;
+    const response = await deleteRequest(BASE_URL + ENDPOINT);
+    this.loading = await loadingWithTimeout(50);
+    return response;
   }
 
 };
