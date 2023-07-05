c0e9cfaa66304417001f7d4fe7909d496de611e2
Andres-Developer <andres.burbano.dev@gmail.com>
2023-07-05 00:27:07 -0500

    mod: card title

--- pre-entrega-2/src/views/AdminDashboard.vue
+++ pre-entrega-2/src/views/AdminDashboard.vue
@@ -2,7 +2,7 @@
   <div class="h1 mt-5 mb-5">Panel de administración</div>
 
   <div class="vw-100 d-flex justify-content-center">
-    <BCard title="Crea o edita nuevos productos" img-src="https://picsum.photos/600/300/?image=25" img-alt="Image" img-top
+    <BCard title="Edita o crea nuevos productos" img-src="https://picsum.photos/600/300/?image=25" img-alt="Image" img-top
       tag="article" style="max-width: 20rem;">
       <BButton @click="() => $router.push('/admin/products-management')" variant="primary" class="btn-chip">Administración
         de Productos</BButton>
