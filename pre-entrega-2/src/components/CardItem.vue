2e60dd26da553f13ccce50a358dfa2bc16b1c8ec
Andres-Developer <andres.burbano.dev@gmail.com>
2023-07-05 16:49:42 -0500

    mod: styles in card itme

--- pre-entrega-2/src/components/CardItem.vue
+++ pre-entrega-2/src/components/CardItem.vue
@@ -1,40 +1,42 @@
 <template>
-  <b-card img-alt="Image" img-top tag="article" class="m-2 card-style">
-    <img loading="lazy" class="product-image" @load="$event.target.style.opacity = 1"
-      :src="product.image + '/?random=' + product.id" alt="" height="180">
-    <div class="h4">{{ product.title }}</div>
-    <b-card-text>
-      <!-- {{ product.description }} -->
-    </b-card-text>
-    <div class="h4"> $ {{ product.price }}</div>
-    <div class="d-flex flex-column gap-2">
-      <b-button variant="primary" :to="{ name: 'product-detail-id', params: { id: product.id } }"
-        class="fs-5 btn-chip p-3 d-flex justify-content-center align-items-center">
-        + info</b-button>
-      <div v-if="Number(product.stock) === 0">
-        <b-button variant="secondary" class="w-100 btn-chip" disabled>
-          <div class="d-flex justify-content-center align-items-center gap-2">
-            <img alt="add to cart" src="./../assets/add-to-cart.svg" width="25">
-            <div class="fs-5 "> Agotado </div>
-          </div>
-        </b-button>
-      </div>
-      <div v-else>
-        <div v-if="checkSelectedProduct">
-          <b-button variant="success" class="w-100 fs-5 btn-chip">Agregada!</b-button>
-          <div type="button" @click="[deleteToCartHandleClick(product.id)]" class="mt-2">🗑️ Eliminar</div>
-        </div>
-        <div v-else>
-          <b-button variant="danger" @click="[addToCartHandleClick(product)]" class="w-100 btn-chip">
+  <div img-alt="Image" tag="article"
+    class="m-2 card-style d-flex flex-column justify-content-between gap-2 border p-3 rounded-3">
+    <div class="container-image-description-card">
+      <img loading="lazy" class="product-image" @load="$event.target.style.opacity = 1"
+        :src="product.image + '/?random=' + product.id" alt="" height="180">
+      <div class="h4">{{ product.title }}</div>
+    </div>
+    <div class="container-body-card">
+      <div class="h4"> $ {{ product.price }}</div>
+      <div class="d-flex flex-column gap-2">
+        <b-button variant="primary" :to="{ name: 'product-detail-id', params: { id: product.id } }"
+          class="fs-5 btn-chip p-3 d-flex justify-content-center align-items-center">
+          + info</b-button>
+        <div v-if="Number(product.stock) === 0" class="btn-container">
+          <b-button variant="secondary" class="w-100 btn-chip" disabled>
             <div class="d-flex justify-content-center align-items-center gap-2">
               <img alt="add to cart" src="./../assets/add-to-cart.svg" width="25">
-              <div class="fs-5 "> Agregar al carrito </div>
+              <div class="fs-5 "> Agotado </div>
             </div>
           </b-button>
         </div>
+        <div v-else class="">
+          <div v-if="checkSelectedProduct">
+            <b-button variant="success" class="w-100 fs-5 btn-chip">Agregada!</b-button>
+            <div type="button" @click="[deleteToCartHandleClick(product.id)]" class="mt-2">🗑️ Eliminar</div>
+          </div>
+          <div v-else class=" btn-container">
+            <b-button variant="danger" @click="[addToCartHandleClick(product)]" class="w-100 btn-chip">
+              <div class="d-flex justify-content-center align-items-center gap-2">
+                <img alt="add to cart" src="./../assets/add-to-cart.svg" width="25">
+                <div class="fs-5 "> Agregar al carrito </div>
+              </div>
+            </b-button>
+          </div>
+        </div>
       </div>
     </div>
-  </b-card>
+  </div>
 </template>
 
 <script>
@@ -82,10 +84,22 @@ export default {
 .card-style {
   width: 18rem;
   min-width: 18rem;
-  height: 26rem;
+  height: 28rem;
 }
 
 .btn-chip {
   height: 2.7rem;
+}
+
+.container-image-description-card {
+  height: 15rem;
+}
+
+.container-body-card {
+  height: 10rem;
+}
+
+.btn-container {
+  height: 5rem;
 }
 </style>
