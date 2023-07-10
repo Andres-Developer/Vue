1ebee47cf2425c1c7c040132960c4c0b15ec7e9f
Andres-Developer <andres.burbano.dev@gmail.com>
2023-07-09 19:52:34 -0500

    mod: styles in loading...

--- pre-entrega-3/src/views/HomePage.vue
+++ pre-entrega-3/src/views/HomePage.vue
@@ -7,7 +7,9 @@
       <CardItem v-for="product in this.productsStore.products" :key="product.id" :product="product" />
     </div>
   </div>
-  <div v-else class="h5 mt-5 pt-5">Loading...</div>
+  <div v-else class="custom-height container d-flex justify-content-center align-items-center">
+    <div class="h5 mt-5 pt-5">Loading...</div>
+  </div>
 </template>
 
 <script>
