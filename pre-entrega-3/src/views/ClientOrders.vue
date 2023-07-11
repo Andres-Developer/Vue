99ffbf645cb440f34914db336890544bc96765c6
Andres-Developer <andres.burbano.dev@gmail.com>
2023-07-11 02:02:22 -0500

    fix: validate user authenticated

--- pre-entrega-3/src/views/ClientOrders.vue
+++ pre-entrega-3/src/views/ClientOrders.vue
@@ -1,59 +1,67 @@
 <template>
-  <div class="h1 mt-5 mb-3">Órdenes de clientes</div>
-  <div to="/" class="btn btn-secondary btn-chip mb-5" @click="() => $router.push('/admin')">Volver al panel de
-    administración
+  <div v-if="this.isUserAuthenticated && this.user?.isAdmin">
+    <div class="h1 mt-5 mb-3">Órdenes de clientes</div>
+    <div to="/" class="btn btn-secondary btn-chip mb-5" @click="() => $router.push('/admin')">Volver al panel de
+      administración
+    </div>
+    <b-table-simple @click="cleanDeletedMessage" v-if="!this.loading" responsive class="container table-width">
+      <b-thead>
+        <b-tr variant="light">
+          <b-th>id cliente</b-th>
+          <b-th>Avatar</b-th>
+          <b-th>Nombres</b-th>
+          <b-th>Total órdenes</b-th>
+          <b-th></b-th>
+          <b-th></b-th>
+        </b-tr>
+      </b-thead>
+      <b-tbody>
+        <b-tr v-for="client in this.clientsWithOrders" :key="client.id" class="align-middle">
+          <b-td><strong>{{ client.id }}</strong></b-td>
+          <b-td sticky-column class="d-flex justify-content-start gap-2 align-items-center">
+            <img :src="client.avatar" alt="client" height="60" class="user-avatar">
+          </b-td>
+          <b-td> {{ client.firstname + " " + client.lastname }}</b-td>
+          <b-td>
+            {{ client.orders.length }}
+          </b-td>
+          <b-td>
+            <b-button @click="$router.push(`/admin/client-orders/client/${client.id}`)" class="btn-chip"
+              variant="warning">
+              Ver órdenes
+            </b-button>
+          </b-td>
+          <b-td>
+          </b-td>
+        </b-tr>
+      </b-tbody>
+      <b-tfoot>
+        <b-tr variant="light">
+          <b-th colspan="3" class="text-end">
+          </b-th>
+          <b-th>
+          </b-th>
+          <b-th></b-th>
+          <b-th></b-th>
+        </b-tr>
+      </b-tfoot>
+    </b-table-simple>
+    <div v-else class="h5 mt-5 pt-5">Loading...</div>
   </div>
-  <b-table-simple @click="cleanDeletedMessage" v-if="!this.loading" responsive class="container table-width">
-    <b-thead>
-      <b-tr variant="light">
-        <b-th>id cliente</b-th>
-        <b-th>Avatar</b-th>
-        <b-th>Nombres</b-th>
-        <b-th>Total órdenes</b-th>
-        <b-th></b-th>
-        <b-th></b-th>
-      </b-tr>
-    </b-thead>
-    <b-tbody>
-      <b-tr v-for="client in this.clientsWithOrders" :key="client.id" class="align-middle">
-        <b-td><strong>{{ client.id }}</strong></b-td>
-        <b-td sticky-column class="d-flex justify-content-start gap-2 align-items-center">
-          <img :src="client.avatar" alt="client" height="60" class="user-avatar">
-        </b-td>
-        <b-td> {{ client.firstname + " " + client.lastname }}</b-td>
-        <b-td>
-          {{ client.orders.length }}
-        </b-td>
-        <b-td>
-          <b-button @click="$router.push(`/admin/client-orders/client/${client.id}`)" class="btn-chip" variant="warning">
-            Ver órdenes
-          </b-button>
-        </b-td>
-        <b-td>
-        </b-td>
-      </b-tr>
-    </b-tbody>
-    <b-tfoot>
-      <b-tr variant="light">
-        <b-th colspan="3" class="text-end">
-        </b-th>
-        <b-th>
-        </b-th>
-        <b-th></b-th>
-        <b-th></b-th>
-      </b-tr>
-    </b-tfoot>
-  </b-table-simple>
-  <div v-else class="h5 mt-5 pt-5">Loading...</div>
+  <div v-else>
+    <div class="h1 mt-5 mb-5 text-danger">No autorizado</div>
+    <LoginUser />
+  </div>
 </template>
 
 <script>
+import LoginUser from './LoginUser.vue';
 import { mapActions, mapGetters } from 'vuex';
-
 
 export default {
   name: 'ClientOrders',
   components: {
+    LoginUser,
   },
   props: {
   },
@@ -68,10 +76,17 @@ export default {
     })();
   },
   methods: {
-    ...mapActions('userModule', ['getUsersFromAPI','getClientOrders'])
+    ...mapActions('userModule', ['getUsersFromAPI', 'getClientOrders'])
   },
   computed: {
     ...mapGetters('userModule', ['getUsersWithOrders', 'getLoading']),
+    ...mapGetters('userModule', ['getUser', 'getIsUserAuthenticate']),
+    user() {
+      return this.getUser;
+    },
+    isUserAuthenticated() {
+      return this.getIsUserAuthenticate;
+    },
     clientsWithOrders() {
       return this.getUsersWithOrders;
     },
--- pre-entrega-3/src/views/ClientOrdersList.vue
+++ pre-entrega-3/src/views/ClientOrdersList.vue
@@ -1,34 +1,43 @@
 <template>
-  <div class="h2 mt-5 mb-3">Órdenes del cliente: </div>
-  <div v-if="!this.loading">
-    <div v-if="this.client">
-      <div class="h4 mb-5 text-success">
-        {{ this.client.firstname + " " + this.client.lastname }}
-        <img :src="this.client.avatar" alt="client" height="60" class="user-avatar">
-      </div>
-      <div to="/" class="btn btn-secondary btn-chip mb-4" @click="() => $router.push('/admin/client-orders')">Volver
-      </div>
-      <div v-if="!this.clientOrders.length" class="h5 mt-5 pt-5 text-secondary"> Todavía no ha creado órdenes de compra
-      </div>
-      <div v-else>
-        <div class="container d-flex flex-column justify-content-center table-width">
-          <OrderTable v-for="order in this.clientOrders" :key="order.id" :order="order" />
+  <div v-if="this.isUserAuthenticated && this.user?.isAdmin">
+    <div class="h2 mt-5 mb-3">Órdenes del cliente: </div>
+    <div v-if="!this.loading">
+      <div v-if="this.client">
+        <div class="h4 mb-5 text-success">
+          {{ this.client.firstname + " " + this.client.lastname }}
+          <img :src="this.client.avatar" alt="client" height="60" class="user-avatar">
         </div>
+        <div to="/" class="btn btn-secondary btn-chip mb-4" @click="() => $router.push('/admin/client-orders')">Volver
+        </div>
+        <div v-if="!this.clientOrders.length" class="h5 mt-5 pt-5 text-secondary"> Todavía no ha creado órdenes de compra
+        </div>
+        <div v-else>
+          <div class="container d-flex flex-column justify-content-center table-width">
+            <OrderTable v-for="order in this.clientOrders" :key="order.id" :order="order" />
+          </div>
 
+        </div>
       </div>
+      <div v-else class="h5 mt-5 pt-5 text-danger"> Cliente no encontrado </div>
     </div>
-    <div v-else class="h5 mt-5 pt-5 text-danger"> Cliente no encontrado </div>
+    <div v-else class="h5 mt-5 pt-5">Loading...</div>
   </div>
-  <div v-else class="h5 mt-5 pt-5">Loading...</div>
+  <div v-else>
+    <div class="h1 mt-5 mb-5 text-danger">No autorizado</div>
+    <LoginUser />
+  </div>
 </template>
 
 <script>
 import OrderTable from '@/components/OrderTable.vue';
+import LoginUser from './LoginUser.vue';
 import { mapActions, mapGetters } from 'vuex';
+
 
 export default {
   name: 'ClientOrdersList',
   components: {
+    LoginUser,
     OrderTable
   },
   props: {
@@ -49,6 +58,13 @@ export default {
   },
   computed: {
     ...mapGetters('userModule', ['getClient', 'getLoading']),
+    ...mapGetters('userModule', ['getUser', 'getIsUserAuthenticate']),
+    user() {
+      return this.getUser;
+    },
+    isUserAuthenticated() {
+      return this.getIsUserAuthenticate;
+    },
     client() {
       return this.getClient;
     },
